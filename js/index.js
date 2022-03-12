43c15e9841c8b35faad21898dbe492c552569489
jhon anderson <apxanderson02@gmail.com>
2022-03-12 17:38:59 -0500

    js identificando las casillas

--- js/index.js
+++ js/index.js
@@ -1,8 +1,34 @@
 window.addEventListener("load", function(e) {
     const app = document.getElementById("app");
 
-    for(let i = 1 ; i<= 9; i++){
-        app.innerHTML +=  `<div class='casillas' id="${i}"></div>`
+  
+    class Jugador {
+        constructor(casillas, nombre){
+            this.casillas = casillas;
+            this.nombre = nombre;
+
+        }
     }
+
+
+
+
+
+
+
+    for(let i = 1 ; i<= 9; i++){
+        app.innerHTML +=  `<div class='casillas' id='${i}'  ></div>`
+   
+    }
+
+    document.querySelectorAll(".casillas").forEach(c=> c.addEventListener("click",(e)=>{
+        console.log(e.target.id)
+    }))
+
+
+    
+
+    const player1 =  new Jugador([],"jhon");
+
 
 })
