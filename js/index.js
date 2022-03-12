45ca2120f1c347622dadfdca0864f61251301939
jhon anderson <apxanderson02@gmail.com>
2022-03-12 17:20:08 -0500

    identificador de las casillas

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function(e) {
     const app = document.getElementById("app");
 
     for(let i = 1 ; i<= 9; i++){
-        app.innerHTML +=  `<div class='casillas'></div>`
+        app.innerHTML +=  `<div class='casillas' id="${i}"></div>`
     }
 
 })
